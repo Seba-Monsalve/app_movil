48daa824ed353704c7114f89fe9f4e1d3b306310
seba-monsalve <sebastian.monsalve@usach.cl>
2019-10-30 23:28:22 -0300

    Avance: Llenado de combo box - Agregar Tienda

--- app/src/main/java/clases/Util.java
+++ app/src/main/java/clases/Util.java
@@ -1,13 +1,37 @@
 package clases;
 
 import android.content.Context;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+
+import java.util.ArrayList;
 
 public class Util {
 
+    final static String ip = "http://192.168.0.6/";
 
-    public static void mostrar(Context context,String msje){
-        Toast.makeText(context,msje ,Toast.LENGTH_SHORT).show();
+    public static final JSONArray jProductos = new JSONArray();
+
+
+    public static void mostrar(Context context, String msje) {
+        Toast.makeText(context, msje, Toast.LENGTH_SHORT).show();
     }
 
 
@@ -15,7 +39,7 @@ public class Util {
 
         boolean validacion = false;
         try {
-            rut =  rut.toUpperCase();
+            rut = rut.toUpperCase();
             rut = rut.replace(".", "");
             rut = rut.replace("-", "");
             int rutAux = Integer.parseInt(rut.substring(0, rut.length() - 1));
@@ -31,8 +55,97 @@ public class Util {
             }
 
         } catch (java.lang.NumberFormatException e) {
-        } catch (Exception e) {
+
         }
+
         return validacion;
     }
+
+
+    public static void getPHP(Context cont, final TextView textView, String req) {
+        String url = ip +"server/conexion.php?peticion=" + req;
+        // Request a string response from the provided URL.
+        RequestQueue queue = Volley.newRequestQueue(cont);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: " + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+        queue.add(stringRequest);
+    }
+
+    public static void getPHP(final Context cont, final Spinner spinner, final String req) {
+        String url = ip+"server/conexion.php?peticion="+req;
+        // Request a string response from the provided URL.
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(cont);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        ArrayList<String> datos = new ArrayList<>();
+                        datos.add("Seleccione una opcion");
+                        try {
+                            JSONArray jarr = new JSONArray(response);
+
+                            for (int i = 0; i < jarr.length(); i++) {
+                                JSONObject job = jarr.getJSONObject(i);
+                                String v1 = job.optString("id");
+                                String v2 = job.optString(req);
+                                datos.add(v1 + ".- " + v2);
+                            }
+
+
+                            ArrayAdapter<String> adapter =
+                                    new ArrayAdapter<String>(cont, android.R.layout.simple_spinner_dropdown_item, datos);
+                            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                            spinner.setAdapter(adapter);
+                        } catch (JSONException ex) {
+                            ex.printStackTrace();
+                        }
+                    }}
+
+                , new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                            }
+
+        });
+        queue.add(stringRequest);
+
+    }
+
+
+    public static JSONArray getString(Context cont, String req) throws JSONException {
+        String url = ip +"server/conexion.php?peticion=" + req;
+        final String [] dato = new String[20];
+        // Request a string response from the provided URL.
+        RequestQueue queue = Volley.newRequestQueue(cont);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                       dato[0] =  response;
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+        Log.d("AQUI","error en el volley gestring");
+            }
+        });
+        queue.add(stringRequest);
+        return new JSONArray(dato[0]);
+    }
+
 }
--- app/src/main/java/com/example/proyecto_app_moviles/agregar_productos_tienda.java
+++ app/src/main/java/com/example/proyecto_app_moviles/agregar_productos_tienda.java
@@ -2,13 +2,66 @@ package com.example.proyecto_app_moviles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.nio.file.Files;
+
+import clases.Util;
+
+import static clases.Util.getPHP;
+import static clases.Util.jProductos;
 
 public class agregar_productos_tienda extends AppCompatActivity {
+
+
+
+    Spinner spn_categoria;
+    Spinner spn_proveedor;
+    Spinner spn_nombre;
+
+    TextView prueba ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_productos_tienda);
+
+
+
+        spn_categoria = findViewById(R.id.spn_categoria);
+        spn_proveedor = findViewById(R.id.spn_proveedor);
+        spn_nombre = findViewById(R.id.spn_nombre);
+        prueba = findViewById(R.id.prueba);
+
+
+
+        spn_categoria.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                Log.d("POÑO","ACA BRO");
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        Util.getPHP(this,spn_nombre, "producto");
+        Util.getPHP(this, spn_categoria, "categoria");
+        Util.getPHP(this, spn_proveedor, "proveedor");
+
+
     }
 }
